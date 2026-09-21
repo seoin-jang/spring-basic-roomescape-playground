62c464e4caf3d91cca86471d355091e542d8760f
장서인 <seoin1893@dankook.ac.kr>
2026-09-21 17:06:11 +0900

    fix : 예외처리 변경

--- src/main/java/roomescape/time/AdminTimeController.java
+++ src/main/java/roomescape/time/AdminTimeController.java
@@ -21,7 +21,7 @@ public class AdminTimeController {
     @PostMapping("/admin/times")
     public ResponseEntity<Time> create(@RequestBody Time time) {
         if (time.getValue() == null || time.getValue().isEmpty()) {
-            throw new RuntimeException();
+            throw new IllegalArgumentException();
         }
 
         Time newTime = timeService.save(time);
