a4fd996878996b93550894c5237ea81429def347
장서인 <seoin1893@dankook.ac.kr>
2026-09-25 16:26:16 +0900

    fix : 4단계 - time 엔티티 변경

--- src/main/java/roomescape/time/Time.java
+++ src/main/java/roomescape/time/Time.java
@@ -1,8 +1,21 @@
 package roomescape.time;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "time")
 public class Time {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "time_value", nullable = false)
     private String value;
+
+    @Column(nullable = false)
+    private boolean deleted = false;
 
     public Time(Long id, String value) {
         this.id = id;
@@ -14,7 +27,15 @@ public class Time {
     }
 
     public Time() {
+    }
 
+    @JsonIgnore
+    public String getTime() {
+        return value;
+    }
+
+    public void delete() {
+        this.deleted = true;
     }
 
     public Long getId() {
