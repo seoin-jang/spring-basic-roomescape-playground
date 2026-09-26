068a5b37d7af26512278355d6af88e1a87776c7c
장서인 <seoin1893@dankook.ac.kr>
2026-09-26 13:59:45 +0900

    test : 4단계 - 테스트 작성

--- src/test/java/roomescape/MissionStepTest.java
+++ src/test/java/roomescape/MissionStepTest.java
@@ -5,9 +5,14 @@ import io.restassured.http.ContentType;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import roomescape.reservation.ReservationResponse;
+import roomescape.time.Time;
+import roomescape.time.TimeRepository;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -107,5 +112,25 @@ public class MissionStepTest {
                    .get("/admin")
                    .then().log().all()
                    .statusCode(200);
+    }
+
+    @DataJpaTest
+    public class JpaTest {
+        @Autowired
+        private TestEntityManager entityManager;
+
+        @Autowired
+        private TimeRepository timeRepository;
+
+        @Test
+        void 사단계() {
+            Time time = new Time("10:00");
+            entityManager.persist(time);
+            entityManager.flush();
+
+            Time persistTime = timeRepository.findById(time.getId()).orElse(null);
+
+            assertThat(persistTime.getTime()).isEqualTo(time.getTime());
+        }
     }
 }
