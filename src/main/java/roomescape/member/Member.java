19e65cacf28412d34e891b2ff1eafedd0a8f02b7
장서인 <seoin1893@dankook.ac.kr>
2026-09-26 13:54:01 +0900

    fix : 4단계 - member 엔티티 변경

--- src/main/java/roomescape/member/Member.java
+++ src/main/java/roomescape/member/Member.java
@@ -1,12 +1,29 @@
 package roomescape.member;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "member")
 public class Member {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private final String name;
-    private final String email;
-    private final String password;
-    private final String role;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+
+    protected Member() {
+    }
 
     public Member(Long id, String name, String email, String password, String role) {
         this.id = id;
