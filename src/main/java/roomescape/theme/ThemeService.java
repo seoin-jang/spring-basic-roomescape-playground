0d77d18ecdc733c9a42610be3dfceaa1ebc06bd2
장서인 <seoin1893@dankook.ac.kr>
2026-09-26 13:53:28 +0900

    fix : 4단계 - theme 연관 클래스 변경

--- src/main/java/roomescape/theme/ThemeService.java
+++ src/main/java/roomescape/theme/ThemeService.java
@@ -3,25 +3,28 @@ package roomescape.theme;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @Service
 public class ThemeService {
 
-    private final ThemeDao themeDao;
+    private final ThemeRepository themeRepository;
 
-    public ThemeService(ThemeDao themeDao) {
-        this.themeDao = themeDao;
+    public ThemeService(ThemeRepository themeRepository) {
+        this.themeRepository = themeRepository;
     }
 
     public List<Theme> findAll() {
-        return themeDao.findAll();
+        return themeRepository.findAllByDeletedFalse();
     }
 
     public Theme save(Theme theme) {
-        return themeDao.save(theme);
+        return themeRepository.save(theme);
     }
 
     public void deleteById(Long id) {
-        themeDao.deleteById(id);
+        Theme theme = themeRepository.findById(id).orElseThrow(NoSuchElementException::new);
+        theme.delete();
+        themeRepository.save(theme);
     }
 }
