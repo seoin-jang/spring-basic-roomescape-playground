6f394447cbc7e0d610f8db8da69bd58cab7c08d1
장서인 <seoin1893@dankook.ac.kr>
2026-09-26 13:54:18 +0900

    fix : 4단계 - member 연관 클래스 변경

--- src/main/java/roomescape/member/MemberService.java
+++ src/main/java/roomescape/member/MemberService.java
@@ -6,30 +6,19 @@ import org.springframework.stereotype.Service;
 @Service
 public class MemberService {
 
-    private final MemberDao memberDao;
+    private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
 
-    public MemberService(MemberDao memberDao, PasswordEncoder passwordEncoder) {
-        this.memberDao = memberDao;
+    public MemberService(MemberRepository memberRepository, PasswordEncoder passwordEncoder) {
+        this.memberRepository = memberRepository;
         this.passwordEncoder = passwordEncoder;
     }
 
     public MemberResponse createMember(MemberRequest memberRequest) {
         String encodedPassword = passwordEncoder.encode(memberRequest.getPassword());
+        Member member = new Member(memberRequest.getName(), memberRequest.getEmail(), encodedPassword, "USER");
+        Member savedMember = memberRepository.save(member);
 
-        Member member = new Member(
-                memberRequest.getName(),
-                memberRequest.getEmail(),
-                encodedPassword,
-                "USER"
-        );
-
-        Member savedMember = memberDao.save(member);
-
-        return new MemberResponse(
-                savedMember.getId(),
-                savedMember.getName(),
-                savedMember.getEmail()
-        );
+        return new MemberResponse(savedMember.getId(), savedMember.getName(), savedMember.getEmail());
     }
 }
