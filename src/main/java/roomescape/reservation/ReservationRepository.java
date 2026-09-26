485cebb8109f635dd449a863e13ea3fbfae2488b
장서인 <seoin1893@dankook.ac.kr>
2026-09-26 16:34:05 +0900

    feat : 5단계 - 예약 조회 메서드 추가

--- src/main/java/roomescape/reservation/ReservationRepository.java
+++ src/main/java/roomescape/reservation/ReservationRepository.java
@@ -4,8 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface ReservationRepository
-        extends JpaRepository<Reservation, Long> {
-
+public interface ReservationRepository extends JpaRepository<Reservation, Long> {
     List<Reservation> findByDateAndThemeId(String date, Long themeId);
+    List<Reservation> findAllByMemberId(Long memberId);
 }
