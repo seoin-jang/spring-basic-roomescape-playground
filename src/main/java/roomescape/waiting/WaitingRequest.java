3c7140b0dcd3197bee7b69cf2e499cef02c1be1f
장서인 <seoin1893@dankook.ac.kr>
2026-09-26 21:23:43 +0900

    feat : 6단계 - WaitingRequest 추가

---
+++ src/main/java/roomescape/waiting/WaitingRequest.java
@@ -0,0 +1,23 @@
+package roomescape.waiting;
+
+public class WaitingRequest {
+
+    private String date;
+    private Long time;
+    private Long theme;
+
+    public WaitingRequest() {
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public Long getTime() {
+        return time;
+    }
+
+    public Long getTheme() {
+        return theme;
+    }
+}
