e9ac8d643258a38fc096c0c479d9b162f617397e
장서인 <seoin1893@dankook.ac.kr>
2026-09-26 13:56:05 +0900

    chore : 4단계 - 스키마 변경

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -12,18 +12,18 @@ VALUES (
            'USER'
        );
 
-INSERT INTO theme (name, description)
-VALUES ('테마1', '테마1입니다.'),
-       ('테마2', '테마2입니다.'),
-       ('테마3', '테마3입니다.');
+INSERT INTO theme (name, description, deleted)
+VALUES ('테마1', '테마1입니다.', false),
+       ('테마2', '테마2입니다.', false),
+       ('테마3', '테마3입니다.', false);
 
-INSERT INTO time (time_value)
-VALUES ('10:00'),
-       ('12:00'),
-       ('14:00'),
-       ('16:00'),
-       ('18:00'),
-       ('20:00');
+INSERT INTO time (time_value, deleted)
+VALUES ('10:00', false),
+       ('12:00', false),
+       ('14:00', false),
+       ('16:00', false),
+       ('18:00', false),
+       ('20:00', false);
 
 INSERT INTO reservation (name, date, time_id, theme_id)
 VALUES ('어드민', '2024-03-01', 1, 1),
