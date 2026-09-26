d13d118c5df9761fd5f04cde6ef89ee39f59ce93
장서인 <seoin1893@dankook.ac.kr>
2026-09-26 16:35:59 +0900

    fix : 5단계 - 스키마 변경

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -25,7 +25,10 @@ VALUES ('10:00', false),
        ('18:00', false),
        ('20:00', false);
 
+INSERT INTO reservation (member_id, name, date, time_id, theme_id)
+VALUES (1, '', '2024-03-01', 1, 1),
+       (1, '', '2024-03-01', 2, 2),
+       (1, '', '2024-03-01', 3, 3);
+
 INSERT INTO reservation (name, date, time_id, theme_id)
-VALUES ('어드민', '2024-03-01', 1, 1),
-       ('어드민', '2024-03-01', 2, 2),
-       ('어드민', '2024-03-01', 3, 3);
+VALUES ('브라운', '2024-03-01', 1, 2);
