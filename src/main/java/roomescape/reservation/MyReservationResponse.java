59383362dbf9677702e77f9a99d9db272cc77a81
장서인 <seoin1893@dankook.ac.kr>
2026-09-26 16:34:37 +0900

    feat : 5단계 - MyReservationResponse 클래스 작성

---
+++ src/main/java/roomescape/reservation/MyReservationResponse.java
@@ -0,0 +1,38 @@
+package roomescape.reservation;
+
+public class MyReservationResponse {
+
+    private final Long reservationId;
+    private final String theme;
+    private final String date;
+    private final String time;
+    private final String status;
+
+    public MyReservationResponse(Long reservationId, String theme, String date, String time, String status) {
+        this.reservationId = reservationId;
+        this.theme = theme;
+        this.date = date;
+        this.time = time;
+        this.status = status;
+    }
+
+    public Long getReservationId() {
+        return reservationId;
+    }
+
+    public String getTheme() {
+        return theme;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+}
